61e66bb3fa703ef5cea721ff3d7236228a6c9735
ch-4ml <fetch@ajou.ac.kr>
2022-06-29 15:01:32 +0900

    chore: Add comments to the application code

--- application/app.js
+++ application/app.js
@@ -7,72 +7,32 @@
 'use strict';
 
 /**
- * Application that shows events when creating and updating an asset
- *   -- How to register a contract listener for chaincode events
- *   -- How to get the chaincode event name and value from the chaincode event
- *   -- How to retrieve the transaction and block information from the chaincode event
- *   -- How to register a block listener for full block events
- *   -- How to retrieve the transaction and block information from the full block event
- *   -- How to register to recieve private data associated with transactions when
- *      registering a block listener
- *   -- How to retreive the private data from the full block event
- *   -- The listener will be notified of an event at anytime. Notice that events will
- *      be posted by the listener after the application activity causing the ledger change
- *      and during other application activity unrelated to the event
- *   -- How to connect to a Gateway that will not use events when submitting transactions.
- *      This may be useful when the application does not want to wait for the peer to commit
- *      blocks and notify the application.
- *
- * To see the SDK workings, try setting the logging to be displayed on the console
- * before executing this application.
+ * 자산을 생성, 수정, 전송, 삭제 시 발생하는 이벤트를 표시하는 응용 프로그램입니다.
+
+ *   -- Contract listener를 등록하여 체인코드 이벤트를 수신하는 방법을 다룹니다.
+ *   -- 체인코드 이벤트로부터 체인코드 이벤트의 이름과 값을 가져오는 방법을 다룹니다.
+ *   -- 체인코드 이벤트로부터 트랜잭션과 블록의 정보를 가져오는 방법을 다룹니다.
+ 
+ *   -- Block listener를 등록하여 모든 블록 이벤트를 수신하는 방법을 다룹니다.
+ *   -- 모든 블록 이벤트로부터 트랜잭션과 블록의 정보를 가져오는 방법을 다룹니다.
+ *   -- 트랜잭션과 관련된 private data를 가져오기 위한 block listener 설정 방법을 다룹니다.
+ *   -- 모든 블록 이벤트로부터 private data를 가져오는 방법을 다룹니다.
+ 
+ *   -- Listener는 언제든지 이벤트를 수신할 수 있습니다.
+ *   -- 이벤트는 transaction commit, block commit 등 원장 변경의 원인이 되는 작업 후에 비동기로 발생합니다.
+ *   -- Transaction commit event를 사용하지 않는 게이트웨이에 연결하는 방법을 다룹니다.
+ *   -- 이 기능은 응용 프로그램이 peer가 블록을 커밋하고 응용 프로그램에 통보할 때까지 기다릴 필요가 없는 경우 유용할 수 있습니다.
+ 
+ * 현재 SDK 실행 로그는 debug.log 파일에 저장됩니다.
+ * SDK의 작동을 실시간으로 확인하려면 이 응용 프로그램을 실행하기 전에 콘솔에 로그가 표시되도록 설정하세요.
  *        export HFC_LOGGING='{"debug":"console"}'
- * See the following on how the SDK is working with the Peer's Event Services
+
+ * SDK가 피어 이벤트 서비스와 상호작용하는 방법
  * https://hyperledger-fabric.readthedocs.io/en/latest/peer_event_services.html
  *
- * See the following for more details on using the Node SDK
+ * 자세한 Node SDK 사용 방법
  * https://hyperledger.github.io/fabric-sdk-node/release-2.2/module-fabric-network.html
  */
-
-// pre-requisites:
-// - fabric-sample two organization test-network setup with two peers, ordering service,
-//   and 2 certificate authorities
-//         ===> from directory test-network
-//         ./network.sh up createChannel -ca
-//
-// - Use the asset-transfer-events/chaincode-javascript chaincode deployed on
-//   the channel "mychannel". The following deploy command will package, install,
-//   approve, and commit the javascript chaincode, all the actions it takes
-//   to deploy a chaincode to a channel.
-//         ===> from directory test-network
-//         ./network.sh deployCC -ccn events -ccp ../asset-transfer-events/chaincode-javacript/ -ccl javascript -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
-//
-// - Be sure that node.js is installed
-//         ===> from directory asset-transfer-sbe/application-javascript
-//         node -v
-// - npm installed code dependencies
-//         ===> from directory asset-transfer-sbe/application-javascript
-//         npm install
-// - to run this test application
-//         ===> from directory asset-transfer-sbe/application-javascript
-//         node app.js
-
-// NOTE: If you see an error like these:
-/*
-
-   Error in setup: Error: DiscoveryService: mychannel error: access denied
-
-   OR
-
-   Failed to register user : Error: fabric-ca request register failed with errors [[ { code: 20, message: 'Authentication failure' } ]]
-
-	*/
-// Delete the /fabric-samples/asset-transfer-sbe/application-javascript/wallet directory
-// and retry this application.
-//
-// The certificate authority must have been restarted and the saved certificates for the
-// admin and application user are not valid. Deleting the wallet store will force these to be reset
-// with the new certificate authority.
-//
 
 // use this to set logging, must be set before the require('fabric-network');
 process.env.HFC_LOGGING = '{"debug": "./debug.log"}';
@@ -103,37 +63,33 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// 피어와 애플리케이션이 상호작용할 게이트웨이를 생성하는 함수
+// 미리 만들어둔 모듈을 이용하여(line 44-45) 네트워크 연결 정보 전달, 지갑 및 인증정보 생성 기능 등 수행
 async function initGatewayForOrg1(useCommitEvents) {
   console.log(`${GREEN}--> Fabric client user & Gateway init: Using Org1 identity to Org1 Peer${RESET}`);
-  // build an in memory object with the network configuration (also known as a connection profile)
-  const ccpOrg1 = buildCCPOrg1();
 
-  // build an instance of the fabric ca services client based on
-  // the information in the network configuration
+  const ccpOrg1 = buildCCPOrg1();
   const caOrg1Client = buildCAClient(FabricCAServices, ccpOrg1, 'ca.org1.example.com');
 
-  // setup the wallet to cache the credentials of the application user, on the app server locally
   const walletPathOrg1 = path.join(__dirname, 'wallet', 'org1');
   const walletOrg1 = await buildWallet(Wallets, walletPathOrg1);
 
-  // in a real application this would be done on an administrative flow, and only once
-  // stores admin identity in local wallet, if needed
   await enrollAdmin(caOrg1Client, walletOrg1, org1);
-  // register & enroll application user with CA, which is used as client identify to make chaincode calls
-  // and stores app user identity in local wallet
-  // In a real application this would be done only when a new user was required to be added
-  // and would be part of an administrative flow
   await registerAndEnrollUser(caOrg1Client, walletOrg1, org1, Org1UserId, 'org1.department1');
 
   try {
     // Create a new gateway for connecting to Org's peer node.
     const gatewayOrg1 = new Gateway();
 
+    // 전달받은 인자 useCommitEvents에 따라 transaction commit event 사용 여부를 결정
     if (useCommitEvents) {
       await gatewayOrg1.connect(ccpOrg1, {
         wallet: walletOrg1,
         identity: Org1UserId,
         discovery: { enabled: true, asLocalhost: true }
+        // eventHandlerOptions: EventStrategies.PREFER_MSPID_SCOPE_ANYFORTX  // Default
+        // 클라이언트 ID가 포함된 organization의 모든 peer에서 트랜잭션 커밋 이벤트를 수신
+        // 클라이언트 ID가 포함된 organization에 peer가 존재하지 않는 경우 네트워크의 모든 peer에서 트랜잭션 커밋 이벤트를 수신
       });
     } else {
       await gatewayOrg1.connect(ccpOrg1, {
@@ -151,6 +107,7 @@ async function initGatewayForOrg1(useCommitEvents) {
   }
 }
 
+// 현재 등록되어 있는 자산 정보가 올바른지 검사하는 함수
 function checkAsset(org, resultBuffer, color, size, owner, appraisedValue, price) {
   console.log(`${GREEN}<-- Query results from ${org}${RESET}`);
 
@@ -197,6 +154,7 @@ function checkAsset(org, resultBuffer, color, size, owner, appraisedValue, price
   }
 }
 
+// Contract listener, block listener를 통해 수신한 이벤트에서 트랜잭션 정보를 가져오는 함수
 function showTransactionData(transactionData) {
   const creator = transactionData.actions[0].header.creator;
   console.log(`    - submitted by: ${creator.mspid}-${creator.id_bytes.toString('hex')}`);
@@ -219,8 +177,8 @@ async function main() {
     let assetKey = `item-${randomNumber}`;
 
     /** ******* Fabric client init: Using Org1 identity to Org1 Peer ******* */
-    const gateway1Org1 = await initGatewayForOrg1(true); // transaction handling uses commit events
-    const gateway2Org1 = await initGatewayForOrg1();
+    const gateway1Org1 = await initGatewayForOrg1(true); // 트랜잭션 커밋 이벤트를 수신하는 게이트웨이
+    const gateway2Org1 = await initGatewayForOrg1(); // 트랜잭션 커밋 이벤트를 수신하지 않는 게이트웨이
 
     try {
       //
@@ -233,27 +191,24 @@ async function main() {
       const contract1Org1 = network1Org1.getContract(chaincodeName);
 
       try {
-        // first create a listener to be notified of chaincode code events
-        // coming from the chaincode ID "events"
+        // Contract listener 정의
         listener = async (event) => {
-          // The payload of the chaincode event is the value place there by the
-          // chaincode. Notice it is a byte data and the application will have
-          // to know how to deserialize.
-          // In this case we know that the chaincode will always place the asset
-          // being worked with as the payload for all events produced.
+          // payload: 체인코드에서 등록한 event에서 전달하는 값
+          // byte 데이터이기 때문에 역직렬화(unmarshal, parse, ...) 필요
+          // 예제에서는 체인코드 event를 통해 항상 현재 처리중인 자산의 정보를 전달하고 있음
           const asset = JSON.parse(event.payload.toString());
           console.log(`${GREEN}<-- Contract Event Received: ${event.eventName} - ${JSON.stringify(asset)}${RESET}`);
           // show the information available with the event
           console.log(`*** Event: ${event.eventName}:${asset.ID}`);
-          // notice how we have access to the transaction information that produced this chaincode event
+          // 체인코드 event를 발생시킨 트랜잭션 정보에 접근할 수 있음
           const eventTransaction = event.getTransactionEvent();
           console.log(`*** transaction: ${eventTransaction.transactionId} status:${eventTransaction.status}`);
           showTransactionData(eventTransaction.transactionData);
-          // notice how we have access to the full block that contains this transaction
+          // 이 트랜잭션을 포함하는 블록 정보에 접근할 수 있음
           const eventBlock = eventTransaction.getBlockEvent();
           console.log(`*** block: ${eventBlock.blockNumber.toString()}`);
         };
-        // now start the client side event service and register the listener
+        // Contract listener 등록
         console.log(`${GREEN}--> Start contract event stream to peer in Org1${RESET}`);
         await contract1Org1.addContractListener(listener);
       } catch (eventError) {
@@ -356,8 +311,9 @@ async function main() {
       try {
         let listener;
 
-        // create a block listener
+        // Block listener 정의
         listener = async (event) => {
+          // Genesis block 이후 새로운 블록이 생성될 때마다 발생하는 이벤트를 감지
           if (firstBlock) {
             console.log(
               `${GREEN}<-- Block Event Received - block number: ${event.blockNumber.toString()}` +
@@ -369,6 +325,7 @@ async function main() {
           } else {
             console.log(`${GREEN}<-- Block Event Received - block number: ${event.blockNumber.toString()}${RESET}`);
           }
+          // 새로 만들어진 블록에 포함된 트랜잭션 이벤트들에 접근할 수 있음
           const transEvents = event.getTransactionEvents();
           for (const transEvent of transEvents) {
             console.log(`*** transaction event: ${transEvent.transactionId}`);
@@ -401,7 +358,8 @@ async function main() {
             }
           }
         };
-        // now start the client side event service and register the listener
+        // Block listener 등록
+        // Private data에 접근할 수 있도록 등록함
         console.log(`${GREEN}--> Start private data block event stream to peer in Org1${RESET}`);
         await network2Org1.addBlockListener(listener, { type: 'private' });
       } catch (eventError) {
@@ -425,12 +383,12 @@ async function main() {
         transaction.setTransient({
           asset_properties: Buffer.from(asset_properties_string)
         });
-        // With the addition of private data to the transaction
-        // We must only send this to the organization that will be
-        // saving the private data or we will get an endorsement policy failure
+        // 트랜잭션에 private data를 추가하는 경우 private data를 저장할 organization에만
+        // 이 정보를 전송해야 함. 그렇지 않으면 endorsement policy failure 발생
         transaction.setEndorsingOrganizations(org1);
-        // endorse and commit - private data (transient data) will be
-        // saved to the implicit collection on the peer
+
+        // Endorse, commit: private data(transient data)가 peer의 implicit collection에 저장됨
+        // implicit collection은 체인코드에 정의되어 있음
         await transaction.submit(assetKey, 'blue', '10', 'Sam', '100');
         console.log(`${GREEN}<-- Submit CreateAsset Result: committed, asset ${assetKey}${RESET}`);
       } catch (createError) {
